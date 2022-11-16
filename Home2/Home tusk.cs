70374c913199ceedf9a047bc57d8447f001a9c5b
ArseniyXLab <117833193+ArseniyXLab@users.noreply.github.com>
2022-11-16 11:57:01 +0300

    Add files via upload

---
+++ Home2/Home tusk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 2, 7, 3, 4 };
+
+            int k = 6;
+            int c = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (k == array[j] + array[i])
+                    {
+                        Console.WriteLine("[{0} , {1}]", i, j);
+                        c++;
+                    }
+                }
+            }
+            if (c == 0)
+                Console.WriteLine("There are no elements  which sum is k");
+
+            Console.ReadKey();
+        }
+    }
+}
